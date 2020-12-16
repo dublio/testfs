f17cf84ceb2f7489de597ccb1613314b3c5974dd
Weiping Zhang <zwp10758@gmail.com>
2020-12-16 11:35:52 -0500

    add LICENSE
    Signed-off-by: Weiping Zhang <zwp10758@gmail.com>

--- mktestfs.c
+++ mktestfs.c
@@ -1,3 +1,24 @@
+/*
+ * testfs - A simple block device based file system
+ *
+ * The license below covers all files distributed with testfs unless otherwise
+ * noted in the file itself.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 3 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2020 Weiping Zhang <zwp10758@gmail.com>
+ *
+ */
 #define _GUN_SOURCE
 #include <stdlib.h>
 #include <sched.h>
